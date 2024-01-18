b0a6cbe7ebdcd3ceacd73840c4a723eb3feaee37
Richiisontheway <mineiriccardo@gmail.com>
2024-01-18 18:23:39 +0100

    footer ended

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,10 +12,43 @@ export default {
 </script>
 
 <template>
-    <footer>
-        FOOTER
+    <footer class="d-flex">
+        <div class="container d-flex align-items-center justify-content-between ">
+            <a href="#">
+                SIGN UP NOW
+            </a>
+            <div class="blue">
+                
+                <h4>
+                    FOLLOW US
+                </h4>
+                <ul>
+                    <li>
+                        <img src="/vue-dc-comics-1\img\footer-facebook.png" alt="">
+                    </li>
+                    <li>
+                        <img src="/vue-dc-comics-1\img\footer-periscope.png" alt="">
+    
+                    </li>
+                    <li>
+                        <img src="/vue-dc-comics-1\img\footer-pinterest.png" alt="">
+    
+                    </li>
+                    <li>
+                        <img src="/vue-dc-comics-1\img\footer-twitter.png" alt="">
+    
+                    </li>
+                    <li>
+                        <img src="/vue-dc-comics-1\img\footer-youtube.png" alt="">
+    
+                    </li>
+                </ul>
+    
+            </div>
+        </div>
     </footer>
 </template>
 
 <style lang="scss" scoped>
+ @use "../assets/scss/footer.scss" as *;
 </style>
